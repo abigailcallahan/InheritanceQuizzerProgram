fbff6f39718d4d1c0ee0ee51e40790d03d829667
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2021-12-13 09:04:15 -0700

    added intro method

--- Inheritance Quizzer Program/src/InheritanceQuizzerProgram.java
+++ Inheritance Quizzer Program/src/InheritanceQuizzerProgram.java
@@ -4,17 +4,41 @@ import java.util.Scanner;
 public class InheritanceQuizzerProgram
 	{
 
+		static Scanner input = new Scanner(System.in);
+		static String enter;
+		
 		public static void main(String[] args)
 			{
 				Food x = new Food( );
 				//Food x = new Pasta( );
 				//Pasta x = new Food( );
 				//Pasta x = new Pasta( );
+				
+				intro();
+				
+				quiz1();
+//				quiz2();
+//				quiz3();
+//				quiz4();
+//				quiz5();
+//				quiz6();
+//				quiz7();
+//				quiz8();
+//				quiz9();
+//				quiz10();
+//				quiz11();
+//				quiz12();
 
 			}
 		
+		public static void intro()
+		{
+			System.out.println("Welcome to the Quizzer Program!");
+			System.out.println("Press enter to start the game");
+			enter = input.nextLine();
+		}
 		
-		public static void runQuiz()
+		public static void quiz1()
 		{
 			
 		}
